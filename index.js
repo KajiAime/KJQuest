ae2a417fcf6c2b68d8b161ffde2605c5a8e50239
KajiAime <ndekaji@gmail.com>
2023-04-24 03:04:16 +0100

    I have implemented the login for the admin and necessary redirects but it still has errors processing the login.

--- index.js
+++ index.js
@@ -1,10 +1,53 @@
 const express = require('express');
 const mysql = require('mysql');
 const ejs = require('ejs');
+const list = require('./classes/linkedlist.js');
+// const passport = require('passport');
+// const LocalStrategy = require('passport-local').Strategy;
+// const session = require('express-session');
 
 const app = express();
 
 app.set('view engine', 'ejs');
+
+// passport.use(new LocalStrategy(
+//     (username, password, done) => {
+//         User.findOne({ username: username }, (err, user) => {
+//             if (err) { return done(err); }
+//             if (!user) { return done(null, false); }
+//             if (!user.verifyPassword(password)) { return done(null, false); }
+//             return done(null, user);
+//         });
+//     }
+// ));
+  
+// passport.serializeUser((user, done) => {
+//     done(null, user.id);
+// });
+  
+// passport.deserializeUser((id, done) => {
+//     User.findById(id, (err, user) => {
+//       done(err, user);
+//     });
+// });
+  
+// app.use(session({
+//     secret: 'mysecretkey',
+//     resave: false,
+//     saveUninitialized: true
+// }));
+  
+// app.use(passport.initialize());
+// app.use(passport.session());
+  
+// app.post('/login',
+//     passport.authenticate('local', {    successRedirect: '/',
+//                                         failureRedirect: '/login' }));
+  
+app.get('/logout', (req, res) => {
+    req.logout();
+    res.redirect('/');
+});
 
 const con = mysql.createConnection({
     database: "kjquest",
@@ -19,7 +62,19 @@ con.connect((err) => {
     }
     console.log('Connected to database!');
 });
-app.get('/', (req, res) => {
+
+// app.use(session({
+//     secret: 'mysecretkey',
+//     resave: false,
+//     saveUninitialized: true
+// }));
+
+app.get('/answer', (req, res) => {
+    if (req.session.views) {
+        req.session.views++;
+    } else {
+        req.session.views = 1;
+    }
     con.query("SELECT * FROM answers WHERE id='1'", (err, results, fields) => {
         if(err){
             console.error('Error running query: ',err);
@@ -29,6 +84,24 @@ app.get('/', (req, res) => {
         console.log(results);
     });
 });
+app.get('/', (req, res) => {
+    res.render('logAdmin');
+})
+app.get('/form', (req, res) =>{
+    res.render('form');
+});
+app.get('/adminSubmit', (req, res) => {
+    const data = req.body.userName;
+    con.query(`SELECT * FROM userinfo WHERE userName = ?`, [data], (error, result) => {
+        if(error) throw error;
+        if(req.body.passWord === result.passWord){
+            res.render('setQuest', {name: result.userName})
+        } else {
+            res.render('loginAdmin', {text: "Incorrect credentials! Try again."})
+        }
+    });
+})
+// app.get('')
 app.use(express.static('public'));
 app.listen(3000, () => {
     console.log('Server started on: http://127.0.0.1:3000');
